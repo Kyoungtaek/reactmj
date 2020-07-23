e16e6e3b78fd2e32b5d6ec4a6e46c39cbfd3b671
Kyoungtaek Oh <okts83@gmail.com>
2020-07-23 14:00:26 -0400

    ch03

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import React, { Fragment } from "react";
-import "./App.css";
+import React from "react";
+import Say from './Say';
 
-function App() {
-  const name = "React";
-  const style = {
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: "48px",
-    fontWeight: "bold",
-    padding: 16,
-  };
-
-  return (
-    <>
-      <div className='react'>{name}</div>
-      <input />
-      {/* Comment */}
-    </>
-  );
-}
+const App = () => {
+  return <Say />;
+};
 
 export default App;
